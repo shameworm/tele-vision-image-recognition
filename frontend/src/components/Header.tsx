bb3e76b7075b0e40775bdca2b1541dd2d291ea12
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2024-05-22 20:24:11 +0300

    ...

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,8 @@
-import { useEffect } from "react";
-import { useTelegram } from "../hooks/useTelegram";
-
-import Button from "./Button";
 
 const Header: React.FC = () => {
-  const { backButton, onClose } = useTelegram();
-
-  useEffect(() => {
-    backButton.show();
-  }, [backButton]);
 
   return (
     <header className="w-full h-10 flex justify-between items-center py-3 px-3">
-      <Button onClick={onClose}>{""}</Button>
       <h2 className="mx-auto text-[var(--tg-theme-text-color)]">
         Hello, please pick the image.
       </h2>
